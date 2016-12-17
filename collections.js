59af4f30279e89334b278d30ba1b8df41dfaa75d
Naor Ami <naoric@gmail.com>
2016-12-17 18:39:12 +0200

    done with collections

--- collections.js
+++ collections.js
@@ -2,21 +2,44 @@ const seeder = require('./seeder').seeder('dummies', 'file');
 const program = require('commander');
 const ajs = require('async');
 const fs = require('fs');
+const _ = require('lodash/fp');
 
 program
   .version('1.0.0')
   .option('-c, --concat', 'Concat example')
   .option('-d, --detect', 'Detect example')
+  .option('-e, --each', 'Each example')
+  .option('-d, --eachOf', 'Each Of example')
+  .option('--every', 'every example')
+  .option('--filter', 'filter example')
+  .option('--map', 'map example')
+  .option('--map-values', 'map-values example')
+  .option('--reduce', 'reduce example')
+  .option('--reject', 'reject example')
   .parse(process.argv);
+
+const existingFileNames = () => [
+  './dummies/file1.txt',
+  './dummies/file2.txt',
+  './dummies/file3.txt'
+];
+
+const nonExistingFileNames = () => ['junk', 'nothing'];
+const mixedFileNames = () =>
+  _.shuffle(existingFileNames().concat(nonExistingFileNames()));
 
 before();
 
 if (program.concat) concat();
-if (program.detect) {
-  detect();
-}
+if (program.detect) detect();
+if (program.each) each();
+if (program.every) every();
+if (program.filter) filter();
+if (program.map) map();
+if (program.reduce) reduce();
+if (program.reject) reject();
 
-after();
+setTimeout(after, 500);
 
 function before() {
   seeder.seedFiles(10);
@@ -27,13 +50,68 @@ function after() {
 }
 
 function concat() {
-  ajs.concat(['./dummies/file1.txt', './dummies/file2.txt', './dummies/file3.txt'],
-      fs.stat,
-      (err, results) => console.log(err, results));
+  ajs.concat(existingFileNames(),
+    fs.stat,
+    (err, results) => console.log(err, results));
 }
 
 function detect() {
-  ajs.detect(['nothing', 'dummies/file1.txt', 'dummies/file2.txt'],
-      (file, callback) => fs.exists(file, isExists => callback(null, isExists)),
-      (err, result) => console.log(result));
+  ajs.detect(mixedFileNames(),
+    (file, callback) => fs.exists(file, isExists => callback(null, isExists)),
+    (err, result) => console.log(result))
+}
+
+function each() {
+  ajs.each(
+    mixedFileNames(),
+
+    (file, cb) =>
+    fs.exists(file, exists =>
+      !exists && cb(`File ${file} does not exist`)),
+
+    err => console.log('something failed .. ', err)
+  );
+}
+
+function every() {
+  ajs.every(
+    mixedFileNames(),
+    (file, cb) => fs.exists(file, exists => cb(null, exists)),
+    (err, result) => result ?
+    console.log('all files exists') :
+    console.log('some files are missing :(')
+  );
+}
+
+function filter() {
+  ajs.filter(
+    mixedFileNames(),
+    (file, cb) => fs.exists(file, exists => cb(null, exists)),
+    (err, results) => console.log('existing files are', results)
+  );
+}
+
+function map() {
+  ajs.map(
+    existingFileNames(),
+    fs.stat,
+    (err, results) => console.log(results)
+  );
+}
+
+function reduce() {
+  ajs.reduce(
+    mixedFileNames(),
+    [],
+    (agg, file, cb) => fs.exists(file, exists => cb(null, exists ? agg.concat(file) : agg)),
+    (err, result) => console.log('memo is ', result)
+  );
+}
+
+function reject() {
+  ajs.reject(
+    mixedFileNames(),
+    (file, cb) => fs.exists(file, exists => cb(null, exists)),
+    (err, result) => console.log('non existing files are', result)
+  );
 }
